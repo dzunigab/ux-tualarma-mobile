0727b35d11d349c5984199e845d3ae0678f36126
David Zúñiga <d.zunigab@uniandes.edu.co>
2023-03-16 04:03:10 -0500

    Adicion de pantallas de crear

--- app/src/main/java/com/davidzub/tualarma/Alarmas.kt
+++ app/src/main/java/com/davidzub/tualarma/Alarmas.kt
@@ -1,7 +1,9 @@
 package com.davidzub.tualarma
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Alarmas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +11,16 @@ class Alarmas : AppCompatActivity() {
         setContentView(R.layout.activity_alarmas)
         if (supportActionBar != null) {
             supportActionBar!!.hide()
+        }
+        val createButton: Button = findViewById(R.id.button4)
+        createButton.setOnClickListener {
+            val intent = Intent(this, CrearAlarma::class.java)
+            startActivity(intent)
+        }
+        val createMButton: Button = findViewById(R.id.button5)
+        createMButton.setOnClickListener {
+            val intent = Intent(this, CrearMedicamento::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/davidzub/tualarma/CrearAlarma.kt
@@ -0,0 +1,98 @@
+package com.davidzub.tualarma
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+
+class CrearAlarma : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_alarma)
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
+        val backButton: ImageButton = findViewById(R.id.imageButton3)
+        backButton.setOnClickListener {
+            val intent = Intent(this, Alarmas::class.java)
+            startActivity(intent)
+        }
+        val guardarButton: Button = findViewById(R.id.buttonInit)
+        guardarButton.setOnClickListener {
+            val intent = Intent(this, Alarmas::class.java)
+            startActivity(intent)
+        }
+        // create instance of the UI elements
+        val pickTimeButton: ImageButton = findViewById(R.id.imageButton4)
+        val previewPickedTimeTextView: TextView = findViewById(R.id.textView5)
+
+        // handle the pick time button to open
+        pickTimeButton.setOnClickListener {
+
+            // instance of MDC time picker
+            val materialTimePicker: MaterialTimePicker = MaterialTimePicker.Builder()
+                // set the title for the alert dialog
+                .setTitleText("SELECT YOUR TIMING")
+                // set the default hour for the
+                // dialog when the dialog opens
+                .setHour(12)
+                // set the default minute for the
+                // dialog when the dialog opens
+                .setMinute(10)
+                // set the time format
+                // according to the region
+                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .build()
+
+            materialTimePicker.show(supportFragmentManager, "CrearAlarma")
+
+            // on clicking the positive button of the time picker
+            // dialog update the TextView accordingly
+            materialTimePicker.addOnPositiveButtonClickListener {
+
+                val pickedHour: Int = materialTimePicker.hour
+                val pickedMinute: Int = materialTimePicker.minute
+
+                // check for single digit hour hour and minute
+                // and update TextView accordingly
+                val formattedTime: String = when {
+                    pickedHour > 12 -> {
+                        if (pickedMinute < 10) {
+                            "${materialTimePicker.hour - 12}:0${materialTimePicker.minute} pm"
+                        } else {
+                            "${materialTimePicker.hour - 12}:${materialTimePicker.minute} pm"
+                        }
+                    }
+                    pickedHour == 12 -> {
+                        if (pickedMinute < 10) {
+                            "${materialTimePicker.hour}:0${materialTimePicker.minute} pm"
+                        } else {
+                            "${materialTimePicker.hour}:${materialTimePicker.minute} pm"
+                        }
+                    }
+                    pickedHour == 0 -> {
+                        if (pickedMinute < 10) {
+                            "${materialTimePicker.hour + 12}:0${materialTimePicker.minute} am"
+                        } else {
+                            "${materialTimePicker.hour + 12}:${materialTimePicker.minute} am"
+                        }
+                    }
+                    else -> {
+                        if (pickedMinute < 10) {
+                            "${materialTimePicker.hour}:0${materialTimePicker.minute} am"
+                        } else {
+                            "${materialTimePicker.hour}:${materialTimePicker.minute} am"
+                        }
+                    }
+                }
+
+                // then update the preview TextView
+                previewPickedTimeTextView.text = formattedTime
+            }
+        }
+    }
+}
